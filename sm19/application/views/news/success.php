f74b20b970e742b676072de283544f90db916763
epdubiso <35310550+epdubiso@users.noreply.github.com>
2019-07-15 22:53:54 -0700

    Initial CI files
    App prior to theme installation

---
+++ sm19/application/views/news/success.php
@@ -0,0 +1,5 @@
+<?php
+//application/views/news/seccess.php
+?>
+<h1>Success!</h1>
+<P>Wouldn't it be so awesome if I could see the new record?</P>
---
+++ sm19/application/views/templates/footer.php
@@ -0,0 +1,7 @@
+<?php
+//applications/views/templates/footer.php
+
+?>
+       <em>&copy; 2019-<?=date('Y')?></em>
+        </body>
+</html>
